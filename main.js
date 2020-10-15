882df7957349ae245298612bfbaec8d1c128adb2
AnastasiiaVA <72625781+AnastasiiaVA@users.noreply.github.com>
2020-10-15 21:38:23 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,77 @@
+//-1-------------------------------------
+
+  /*
+const name = prompt('Write your name and surname');
+alert('Good morning, ' + name);
+*/
+//-2-------------------------------------
+
+ /*
+const year = parseInt(prompt('Your year of birth')) ;
+const now_year = 2020;
+let num = now_year-year;
+alert('You are ' + num);
+*/
+//-3-------------------------------------
+
+ /*
+const side = parseInt(prompt('Write side length of a square '));
+alert('P = ' + side*4 + '  S = ' + side**2);
+*/
+
+//-4-------------------------------------
+
+  /*
+const radius = parseInt(prompt('Write a circle radius '));
+alert('  S = ' + Math.PI*Math.pow(radius,2));
+*/
+
+//-5-------------------------------------
+
+ /*
+const km = parseInt(prompt('Write km between cities ')) ;
+const hours = parseInt(prompt('Write hours you wish to spend ')) ;
+alert('you must move with speed -  ' + km/hours +'km/h');
+*/
+
+//-6-------------------------------------
+
+ /*
+const dollar = parseInt(prompt('Write $ ') );
+const grn = 28.21;
+const euro = 0.85;
+alert('€ = ' + dollar*euro + '    ₴ = '+ dollar*grn);
+ */
+ 
+//-7-------------------------------------
+
+  /*
+const Gb = parseInt(prompt('Write flash drive volume in Gb '))*1024;
+let i = Gb / 820;
+alert('You can download ' + Math.floor(i) + ' files (820Mb)');
+*/
+
+
+//-8-------------------------------------
+
+/*
+const cash = parseInt(prompt('Write your cash '));
+const price = parseInt(prompt('Write a price of chocolate bar '));
+let i = cash / price, j = cash % price;
+alert('You can buy ' + Math.floor(i) + ' bars and '+ j+ ' grn left');
+*/
+
+//-9-------------------------------------
+/*
+
+let number = parseInt(prompt('Write a three-digit number : ' ));
+let back_number = 0, i = 0;
+while(number >= 1){
+    i = Math.floor(number%10);
+    number = number/10;
+    back_number = back_number*10;
+    back_number = back_number + i;
+}
+alert('Your new number is  ' + back_number);
+*/
+
